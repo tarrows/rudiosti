ef5daf25aa97c009f6479188db911ca059d5bd41
tarrows <rem_tate@yahoo.co.jp>
2021-08-17 06:57:36 +0900

    try write

--- src/io.rs
+++ src/io.rs
@@ -1,4 +1,6 @@
 use hound;
+use std::f32::consts::PI;
+use std::i16;
 
 pub fn read(file_path: &str) -> Result<(), hound::Error> {
   let mut reader = hound::WavReader::open(file_path)?;
@@ -9,6 +11,20 @@ pub fn read(file_path: &str) -> Result<(), hound::Error> {
   });
 
   println!("RMS is {}", (sqr_sum / reader.len() as f64).sqrt());
+
+  Ok(())
+}
+
+pub fn write(spec: hound::WavSpec) -> Result<(), hound::Error> {
+  let mut writer = hound::WavWriter::create("sine.wav", spec)?;
+
+  for t in (0..spec.sample_rate).map(|x| x as f32 / spec.sample_rate as f32) {
+    let sample = (t * 440.0 * 2.0 * PI).sin();
+    let amplitude = i16::MAX as f32;
+    writer.write_sample((sample * amplitude) as i16)?;
+  }
+
+  writer.finalize()?;
 
   Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use hound;
 use rudiosti::cli::opts;
-use rudiosti::io::read;
+use rudiosti::io::{read, write};
 
 fn main() {
   let args = opts().get_matches();
   let input = args.value_of("INPUT").unwrap();
   let _ = read(input); // "data/example.wav"
+
+  let spec = hound::WavSpec {
+    channels: 1,
+    sample_rate: 44100,
+    bits_per_sample: 16,
+    sample_format: hound::SampleFormat::Int,
+  };
+
+  write(spec).unwrap();
 }
